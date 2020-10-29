e8b4f3688a14a8ea81b8fd43f53aa31cf134fd85
guruteja <guruteja@olivecrypto.com>
2020-10-30 00:31:52 +0530

    initial commit .

---
+++ src/main/java/com/guru/Java8TopicsApplication.java
@@ -0,0 +1,13 @@
+package com.guru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8TopicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8TopicsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/guru/map/MapExample.java
@@ -0,0 +1,77 @@
+package com.guru.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExample {
+	public static int init = 1;
+	public static Map map = new HashMap<>();
+
+	public static void main(String[] args) {
+
+		String[] arr = new String[] { "guru", "teja", "reddy", "sri" };
+		for (String name : arr) {
+			map.put(map.size() + 1, name);
+		}
+
+		// printing map 
+		System.out.println("printing map in ascending order");
+		map.entrySet()
+		   .stream()
+		   .forEach(System.out::println);
+		
+		
+		// map in asending order by values
+		System.out.println("map in ascending order by values");
+		map
+		.entrySet()
+		.stream()
+		.sorted(Map.Entry.comparingByValue()) // asending order by value
+		.forEach(System.out::println);
+		
+	
+		// map in asending order by keys
+		System.out.println("map in ascending order by keys");
+		map
+		.entrySet()
+		.stream()
+		.sorted(Map.Entry.comparingByKey())// asending order by key
+		.forEach(System.out::println);
+		
+		
+		System.out.println("printing map in dcending order");
+		map
+		.entrySet()
+		.stream()
+		.sorted(Map.Entry.comparingByKey().reversed())// dcending order by key
+		.forEach(System.out::println);
+
+		System.out.println("printing map in dcending order by value");
+		map
+		.entrySet()
+		.stream()
+		.sorted(Map.Entry.comparingByValue().reversed())// dcending order by value
+		.forEach(System.out::println);
+		
+		
+		/**
+		 * sorting the values by map value length
+		 */
+		System.out.println("sorting the values by map value length");
+		map
+		.entrySet()
+		.stream()
+		//.filter(x -> "something".equals(x.getValue()))
+		.forEach(System.out::println);
+		
+		Map<Integer,String> newmap = map;
+		System.out.println("new map ");
+		newmap.entrySet()
+		.stream()
+		.filter(x ->x.getValue().length()>3)
+		.forEach(System.out::println);
+	
+		
+	}
+
+}
